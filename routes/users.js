c190f3f8f277ef24b231bcb32bb178b940e142f0
almisfer <you@example.com>
2020-12-02 01:18:50 +0300

    R

--- routes/users.js
+++ routes/users.js
@@ -45,27 +45,28 @@ router.get("/login", (req, res) => {
 
 
 router.get('/index', (req,res) => {
-  var json = JSON.parse(req.user);
-  console.log('this is therapistid', json['therapist_id']);
+  res.render('index');
+  // var json = JSON.parse(req.user);
+  // console.log('this is therapistid', json['therapist_id']);
 
-  player.findAll({ raw : true,
-      where: {
-          therapist_FK: json['therapist_id'],
-     }
-   })
-  .then( (players) => {
-      console.log(players);
-          //Get total registered patients 
-      player.count({
-          where: {
-              therapist_FK: json['therapist_id'],
-          }
-      }).then( (number) => {
-      console.log('number_of_patients', number);
-      res.render("index", {layout: "layout" , count:number, data:players , user: json, title: "Home"});
-      }).catch((error) => console.log(error));
+  // player.findAll({ raw : true,
+  //     where: {
+  //         therapist_FK: json['therapist_id'],
+  //    }
+  //  })
+  // .then( (players) => {
+  //     console.log(players);
+  //         //Get total registered patients 
+  //     player.count({
+  //         where: {
+  //             therapist_FK: json['therapist_id'],
+  //         }
+  //     }).then( (number) => {
+  //     console.log('number_of_patients', number);
+  //     res.render("index", {layout: "layout" , count:number, data:players , user: json, title: "Home"});
+  //     }).catch((error) => console.log(error));
 
-  }).catch( err => console.log(err));
+  // }).catch( err => console.log(err));
 });
 
 
