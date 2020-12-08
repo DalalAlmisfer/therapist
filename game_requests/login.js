82f2ecc7d03d95217ecb58572ab8e3a4fd60fabc
almisfer <dalalalmisfer@gmail.com>
2020-12-08 03:33:52 +0300

    more

--- game_requests/login.js
+++ game_requests/login.js
@@ -4,71 +4,72 @@ const players = require("../models/player");
 const enviroment = require("../models/enviroment");
 const router = express.Router();
 
-router.get("/login", function (req, res, next) {
-    var user_id = req.params;
-    console.log(user_id);
-    res.send(user_id);
+router.get("/login/:email", function (req, res, next) {
+    // var user_id = req.params;
+    // console.log(user_id);
+    // res.send(user_id);
 
+  var id = req.params.email;
+  var leng = id.length;
+  var sub = id.substring(1, leng);
 
-//   var id = req.params.username;
-//   var leng = id.length;
-//   var sub = id.substring(1, leng);
-
-//   players
-//     .findOne({
-//       where: {
-//         email: sub,
-//       },
-//     })
-//     .then((result) => {
-//       console.log(result.player_id);
-//       res.send(`this is id ${result.player_id}`);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.send("err");
-//     });
+  players
+    .findOne({
+      where: {
+        email: sub,
+      },
+    })
+    .then((result) => {
+      console.log(result.player_id);
+      res.send(`this is id ${result.player_id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("err");
+    });
 
 });
 
-router.post("/login", (req, res) => {
-    var user_id = req.params;
-    console.log(user_id);
-    res.send(user_id);
-//     var leng = id.length;
-//     var sub = id.substring(1, leng);
+router.post("/login/:email", (req, res) => {
+    // var user_id = req.params;
+    // console.log(user_id);
+    // res.send(user_id);
 
-//   players
-//     .findOne({
-//         where: {
-//             email: sub,
-//           },
-//     })
-//     .then((user) => {
-//       if (user) {
-//         players
-//           .update(
-//             { islogged_in: 1 },
-//             {
-//               where: {
-//                 email: sub,
-//               },
-//             }
-//           )
-//           .then((result) => {
-//             console.log(result);
-//           })
-//           .catch((err) => {
-//             console.log(err);
-//           });
-//       } else {
-//         console.log("the user dosent exit");
-//       }
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.send("err");
-//     });
+    var id = req.params.email;
+    var leng = id.length;
+    var sub = id.substring(1, leng);
+
+  players
+    .findOne({
+        where: {
+            email: sub,
+          },
+    })
+    .then((user) => {
+      if (user) {
+        players
+          .update(
+            { islogged_in: 1 },
+            {
+              where: {
+                email: sub,
+              },
+            }
+          )
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        console.log("the user dosent exit");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("err");
+    });
 });
 
 module.exports = router;
