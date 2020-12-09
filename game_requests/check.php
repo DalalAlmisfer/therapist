6df4639ea0f4d9cdb39f8760f6aa058372c2e834
almisfer <dalalalmisfer@gmail.com>
2020-12-09 19:28:11 +0300

    mo

--- game_requests/check.php
+++ game_requests/check.php
@@ -1,37 +1,50 @@
 
   <?php
 
-         if(isset($_POST["user"])){
+   if (isset($_POST["user"])) {
 
 
 
-$user = addslashes($_POST['user']);
+      $user = addslashes($_POST['user']);
 
-            // Create connection
-            $conn = new mysqli("localhost", "root", "","anees_DB");
+      // Create connection
+      $conn = new mysqli("localhost", "root", "", "anees_DB");
 
-            // Check connection
-            if ($conn->connect_error) {
-               die("Connection failed: " . $conn->connect_error);
-            }
-
-
-            $userinfo = mysqli_fetch_array(mysqli_query($conn,"SELECT * from players where player_id='$user' "));
-
-$schoolcontrol = mysqli_fetch_array(mysqli_query($conn,"SELECT * from enviroments where player_FK='$user' and title='school'"));
-
-			if($schoolcontrol){$school=1;}	else {$school=0;}
-
-$gardencontrol = mysqli_fetch_array(mysqli_query($conn,"SELECT * from enviroments where player_FK='$user' and title='garden'"));
-
-			if($gardencontrol){$garden=1;}	else {$garden=0;}
+      // Check connection
+      if ($conn->connect_error) {
+         die("Connection failed: " . $conn->connect_error);
+      }
 
 
-$marketcontrol = mysqli_fetch_array(mysqli_query($conn,"SELECT * from enviroments where player_FK='$user' and title='market'"));
+      $userinfo = mysqli_fetch_array(mysqli_query($conn, "SELECT * from players where player_id='$user' "));
 
-			if($marketcontrol){$market=1;}	else {$market=0;}
+      $schoolcontrol = mysqli_fetch_array(mysqli_query($conn, "SELECT * from enviroments where player_FK='$user' and title='school'"));
 
-echo $userinfo["first_name"].','.$userinfo["last_name"].','.$userinfo["gander"].','.$school.','.$garden.','.$market;
-		 }
+      if ($schoolcontrol) {
+         $school = 1;
+      } else {
+         $school = 0;
+      }
 
-      ?>
+
+      $gardencontrol = mysqli_fetch_array(mysqli_query($conn, "SELECT * from enviroments where player_FK='$user' and title='garden'"));
+
+      if ($gardencontrol) {
+         $garden = 1;
+      } else {
+         $garden = 0;
+      }
+
+
+      $marketcontrol = mysqli_fetch_array(mysqli_query($conn, "SELECT * from enviroments where player_FK='$user' and title='market'"));
+
+      if ($marketcontrol) {
+         $market = 1;
+      } else {
+         $market = 0;
+      }
+
+      echo $userinfo["first_name"] . ',' . $userinfo["last_name"] . ',' . $userinfo["gander"] . ',' . $school . ',' . $garden . ',' . $market;
+   }
+
+   ?>
--- game_requests/login.js
+++ game_requests/login.js
@@ -21,47 +21,56 @@ router.get("/login", function (req, res, next) {
     })
     .catch((err) => {
       console.log(err);
-      res.send("err");
+      res.send(`this is user email: ${user_info.user}`);
     });
 
 });
 
 router.post("/login", (req, res) => {
-    var user_email = req.query;
-    console.log(user_email.user);
+    //grab the user info from the url 
+    var user_info = req.query;
 
-  players
-    .findOne({
-        where: {
-            email: user_email.user,
-          },
-    })
-    .then((user) => {
-      if (user) {
-        players
-          .update(
-            { islogged_in: 1 },
-            {
-              where: {
-                email: user_email.user,
-              },
-            }
-          )
-          .then((result) => {
-            console.log(result);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } else {
-        console.log("the user dosent exit");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send(`this is user email: ${user_email.user}`);
+    console.log(user_info);
+    res.send(user_info);
 
-    });
+
+  //   //find the user email in players table
+  //  players.findOne({
+  //       where: {
+  //           email: user_info.user,
+  //         },
+  //   })
+  //   .then((user) => {
+
+  //     //if the user exist
+  //     if (user) {
+  //       //update islogged_in feild in players table
+  //       players.update({ islogged_in: 1 },
+  //           {
+  //             where: {
+  //               email: user_info.user,
+  //             },
+  //           }
+  //         )
+  //         .then((result) => {
+  //           console.log(result);
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //         });
+  //     } else {
+  //       console.log("no update happend to the user");
+  //     }
+
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     //if the user dosent exit in players table print this
+  //     res.send(`this is user email: ${user_info.user}`);
+
+  //   });
+
+  
 });
 
 module.exports = router;
