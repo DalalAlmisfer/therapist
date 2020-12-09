1958798540be9ff3fbca9ce8aa4914878032f134
almisfer <dalalalmisfer@gmail.com>
2020-12-09 20:08:57 +0300

    mo

--- check.php
+++ check.php
@@ -8,7 +8,7 @@
       $user = addslashes($_POST['user']);
 
       // Create connection
-      $conn = new mysqli("localhost", "root", "", "anees_DB");
+      $conn = new mysqli("eu-cdbr-west-03.cleardb.net", "b630bdd6b6e1b9", "d159c434","heroku_195f706910a16f0");
 
       // Check connection
       if ($conn->connect_error) {
--- dialog.php
+++ dialog.php
@@ -9,14 +9,15 @@ $user = addslashes($_POST['user']);
 $table = addslashes($_POST['table']);
 
             // Create connection
-            $conn = new mysqli("localhost", "root", "","anees_DB");
+            $conn = new mysqli("eu-cdbr-west-03.cleardb.net", "b630bdd6b6e1b9", "d159c434","heroku_195f706910a16f0");
 
             // Check connection
             if ($conn->connect_error) {
                die("Connection failed: " . $conn->connect_error);
             }
 
-$control = mysqli_fetch_array(mysqli_query($conn,"SELECT * from enviroments where player_FK='".$user."' "));
+
+$control = mysqli_fetch_array(mysqli_query($conn,"SELECT * from enviroments where player_FK='".$user."' "));
 
 $count = 1+ $control["$table"];
 mysqli_query($conn,"UPDATE enviroments set $table='$count' where  player_FK='$user' ");
