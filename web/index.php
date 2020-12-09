0aef844e363016e1400edcc9cb831dad81644059
almisfer <dalalalmisfer@gmail.com>
2020-12-09 20:47:48 +0300

    mo

--- web/index.php
+++ web/index.php
@@ -1,28 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Heroku Multipack Test</title>
+<style>
+        #env-selecter {
+          background-color: #EEF1FC  !important;
+          box-sizing: border-box;
+                box-shadow: 0 15px 25px rgba(0,0,0,.6);
+                border-radius: 10px;
+               padding: 10px;
+        }
 
-    <!-- Bootstrap -->
-    <link href="vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        .selecter {
+          margin-bottom: 10px;
+          position: relative;
+        }
+</style>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="container">
-    <h1>Hello, world! <small>PHP + Node.js on Heroku</small></h1>
-    
-    <p>Running <strong class="text-success">PHP/<?=PHP_VERSION?></strong> on Heroku using the <a href="https://github.com/heroku/heroku-buildpack-php">standard PHP buildpack</a>, serving a site using <a href="http://getbootstrap.com">Bootstrap</a> which was downloaded by <a href="http://bower.io">Bower</a>, which in turn was installed by the <a href="https://github.com/heroku/heroku-buildpack-nodejs">standard Node.js buildpack</a>.</p>
-    
-    <p>Both put together using <a href="https://github.com/heroku/heroku-buildpack-multi">heroku/heroku-buildpack-multi</a>.</p>
-    
-    <p>Source: <a href="https://github.com/dzuelke/heroku-multipack-nodejs-php-example">dzuelke/heroku-multipack-nodejs-php-example</a>
-  </body>
-</html>
+
+
+<div class="wrapper-2">
+
+    <img src="/images/Anees logo.png" alt="logo" class="logo" />
+
+<div id="env-selecter">
+
+    <div class="selecter">
+        <form action="" method="POST">
+            User type: 
+            <% if (typeof usertype != 'undefined') { %> 
+            <button class="uk-button uk-button-default uk-text-small" type="button">  <%= usertype %> 
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3.204 5L8 10.481 12.796 5H3.204zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                  </svg>
+            </button>
+        <% } else { %> 
+            <button class="uk-button uk-button-default uk-text-small" type="button">  select your user type
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3.204 5L8 10.481 12.796 5H3.204zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                  </svg>
+            </button>
+        <% } %> 
+            <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 1000" style="color: white;">
+                            <ul class="uk-nav uk-dropdown-nav">
+                                 <li class="uk-active"> <a href="/users/registerTherapist"> Therapist </a> </li>
+                                 <li class="uk-active"> <a  href="/usersAdmain/registerAdmain"> Admin </a> </li>
+                </ul>
+            </div>
+        </form>
+    </div>
+
+    <% if (typeof chosen != 'undefined') { %> 
+        <% if ( chosen == 'therapist_reg') { %> 
+            <%- include register %> 
+        <%} else if ( chosen == 'admin_reg') {  %> 
+             <%- include registerAdmain %> 
+        <% } else if (chosen == 'therapist_login') { %>
+            <%- include login %> 
+        <% } else if (chosen == 'admin_login') { %>
+            <%- include loginAdmain %> 
+        <% } else if ( chosen == 'nothing' ){ %> 
+            <%- include hello %> 
+        <% } }%> 
+
+
+
+</div>
+
+
+</div>
