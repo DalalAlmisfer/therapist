a3724e281957e2a80ad7b6701a5f4710fd6d90af
almisfer <dalalalmisfer@gmail.com>
2020-12-13 06:38:56 +0300

    m

--- routes/Admain/usersAdmain.js
+++ routes/Admain/usersAdmain.js
@@ -79,65 +79,62 @@ router.post('/loginAdmain',  (req, res) => {
       subject: "Anees Team - confirmation", // Subject line
       text: ` Hello Again, ${link}`, // plain text body
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-          <html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
-          <head>
-          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-          <title> Confirmation Email  </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          
-          <style type="text/css">
-          a[x-apple-data-detectors] {color: inherit !important;}
-          </style>
-          
-          </head>
-          <body style="margin: 0; padding: 0;">
-          <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
-          <tr>
-          <td style="padding: 20px 0 30px 0;">
-          
-          <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; border: 1px solid #cccccc;">
-          <tr>
-          <td align="center" bgcolor="#0000" style="padding: 40px 0 30px 0;">
-              <img src="/images/Anees logo.png" alt="logo" class="logo"/>
+      <html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
+      <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <title> Confirmation Email  </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      
+      <style type="text/css">
+      a[x-apple-data-detectors] {color: inherit !important;}
+      </style>
+      
+      </head>
+      <body style="margin: 0; padding: 0;">
+      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td style="padding: 20px 0 30px 0;">
+      
+      <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; border: 1px solid #cccccc;">
+      <tr>
+      <td align="center" bgcolor="#0000" style="padding: 40px 0 30px 0;">
+          <img src="../public/images/Anees logo.png" alt="logo" class="logo"/>
+      </td>
+      </tr>
+      <tr>
+      <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+      <tr>
+      <td style="color: #153643; font-family: Arial, sans-serif;">
+      <h1 style="font-size: 24px; margin: 0;">
+          Hello, Again </h1>
+      </td>
+      </tr>
+      <tr>
+      <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 20px 0 30px 0;">
+      <p style="margin: 0;">
+      <a href=${link}> ${link} </a>
+      </p>
+      </td>
+      </tr>
+      
+      </td>
+      </tr>
+      </table>
+      <tr>
+          <td align="center" bgcolor="#0000" style="padding: 40px 0 30px 0; color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;">
+              <p style="margin: 0;">&reg; Anees 2020 <br/>
+              King Saud University. </p>
+      
           </td>
-          </tr>
-          <tr>
-          <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
-          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
-          <tr>
-          <td style="color: #153643; font-family: Arial, sans-serif;">
-          <h1 style="font-size: 24px; margin: 0;">
-               </h1>
-          </td>
-          </tr>
-          <tr>
-          <td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 24px; padding: 20px 0 30px 0;">
-          <p style="margin: 0;">
-          <h3> Hello Again, <h3>
-            <a href='${link}'> ${link} </a> 
-
-
-          </p>
-          </td>
-          </tr>
-          
-          </td>
-          </tr>
-          </table>
-          <tr>
-              <td align="center" bgcolor="#0000" style="padding: 40px 0 30px 0; color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;">
-                  <p style="margin: 0;">&reg; Anees 2020 <br/>
-                  <a href="#" style="color: #ffffff;">Unsubscribe</a> King Saud University. </p>
-          
-              </td>
-          </tr>
-          </td>
-          </tr>
-          </table>
-          
-          
-          </body>
-          </html>`, // html body
+      </tr>
+      </td>
+      </tr>
+      </table>
+      
+      
+      </body>
+      </html>`, // html body
     });
   }
 
